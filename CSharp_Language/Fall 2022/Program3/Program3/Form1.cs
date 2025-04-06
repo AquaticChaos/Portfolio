1412b9adf4ea581439e8854925e9416e2a62047d
Jacob Palmer <63919544+AquaticChaos@users.noreply.github.com>
2025-04-06 19:14:11 -0400

    Add files via upload

---
+++ CSharp_Language/Fall 2022/Program3/Program3/Form1.cs
@@ -0,0 +1,154 @@
+﻿/*
+    CIS-199-50-4228 Program #3
+    Due: 11/13/2022 (Formerly 11/10/2022)
+    ID: F2233
+    Purpose: To calculate the loan interest, credit rating, and down payment based on
+        user input of credit score & loan amount and type through the use of parallel arrays and
+        range matching.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program3
+{
+    public partial class program3Form : Form
+    {
+        public program3Form()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            //Values used for later assuming no errors.
+            string outputCSRating = "Null";
+            double outputCSInterest = -1, outputLTBaseRate = -1, outputLADownPerc = -1;
+
+            //For displaying percentage and currency
+            CultureInfo ci = new CultureInfo("en-us");
+
+            //Arrays to compare input credit score to max & min values...
+            int[] creditScoreMin = {300, 580, 670, 740, 800}; //Allowed minimum of 300
+            int[] creditScoreMax = {579, 669, 739, 799, 850}; //Allowed minimum of 300
+            //And calculate the rating & loan interest amounts.
+            string[] creditScoreRating = {"Poor", "Fair", "Good", "Very Good", "Excellent"};
+            double[] creditScoreInterest = {0.08014, 0.07373, 0.07063, 0.06149, 0.059};
+
+            //Inputted loan type won't need arrays, a simple case-switch will suffice later.
+
+            //Similarly, arrays to compare input loan amount to min & max values...
+            double[] loanAmountMin = {1, 500, 2000, 10000, 100000}; //Allowed minimum of $1
+            double[] loanAmountMax = {499, 1999, 9999, 99999, 500000}; //Allowed maximum of $500K
+            //And again calculate the down payment percent.
+            double[] loanAmountDown = {0, .1, .15, .1, .05};
+
+            //Check if anything is wrong, display message at end if so display message with problems.
+            Boolean isAllValid = true;
+            string errorText = "Please check over the following and try again:\n";
+
+
+
+            //Inputted loan type
+            if (loanTypeCombo.SelectedIndex >= 0) //If selection made...
+            {
+                switch(loanTypeCombo.Text)
+                {
+                    case "Home":
+                        outputLTBaseRate = 1; break; //100%
+                    case "Auto":
+                        outputLTBaseRate = 1.3; break; //130%
+                    case "Unsecured":
+                        outputLTBaseRate = 2; break; //200%
+                    default:
+                        isAllValid = false;
+                        errorText += "\nAn invalid loan type was selected";
+                        break; //Failsafe if somehow none of above
+                }
+            } else
+            { //If no selection made
+                isAllValid = false;
+                errorText += "\nA loan type was not selected.";
+            }
+
+            //Inputted credit score
+            if (Int32.TryParse(creditScoreTextbox.Text, out int creditScoreInput)) //If int...
+            {
+                Boolean creditScoreInRange = false;
+                for (int i = 0; i < loanAmountMin.Length; i++) //Go through array loop, arrays have same length
+                {
+                    if (creditScoreInput >= creditScoreMin[i] && creditScoreInput <= creditScoreMax[i])
+                    { //Found range? Exit for-loop early and set values for later.
+                        creditScoreInRange = true;
+                        outputCSRating = creditScoreRating[i];
+                        outputCSInterest = creditScoreInterest[i];
+                        break;
+                    }
+                }
+
+                if (!creditScoreInRange)
+                {
+                    isAllValid = false;
+                    errorText += "\nThe provided credit score is outside the range 300 - 850.";
+                }
+            }
+            else //If not int...
+            {
+                //Error found, add to error text that number not given
+                isAllValid = false;
+                errorText += "\nA number was not given for your credit score.";
+            }
+
+
+            //Inputted loan amount
+            if (double.TryParse(loanAmountTextbox.Text, out double loanAmountInput))
+            { //If valid input
+                Boolean loanAmountInRange = false;
+                for(int i = 0; i < loanAmountMin.Length; i++) //Go through array loop, arrays have same length
+                {
+                    if(loanAmountInput >= loanAmountMin[i] && loanAmountInput  <= loanAmountMax[i])
+                    { //Found range? Exit for-loop early and set values for later.
+                        loanAmountInRange = true;
+                        outputLADownPerc = loanAmountDown[i];
+                        break;
+                    }
+                    
+                }
+
+                if (!loanAmountInRange)
+                {
+                    isAllValid = false;
+                    errorText += "\nThe provided loan amount is outside the range 1 - 500,000.";
+                }
+            } else
+            {//If invalid input
+                isAllValid = false;
+                errorText += "\nAn invalid loan amount was made.";
+            }
+
+            if(isAllValid) //No errors?
+            {
+                //Calculate final loan interest from credit score & loan type, display as percent
+                double outputInterestFinal = outputCSInterest * outputLTBaseRate;
+                loanInterestOutput.Text = outputInterestFinal.ToString("P", ci);
+
+                //Display credit rating recieved from input credit score
+                creditRatingOutput.Text = outputCSRating; //No need to do ToString, already one.
+
+                //Calculate down payment from loan amount, display as currency
+                double downPaymentFinal = outputLADownPerc * loanAmountInput;
+                downPaymentOutput.Text = downPaymentFinal.ToString("C", ci);
+            }
+            else //Errors found, show message.
+                 {MessageBox.Show(errorText);}
+        }
+    }
+}
---
+++ CSharp_Language/Fall 2022/Program4/Program4/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+    CIS-199-50-4228 Program #4
+    Due: 12/2/22 (formerly 11/30, then 12/1)
+    ID: F2233
+    Purpose: Using classes, objects, and constructors; create five different products that will display their
+        supplier and product name, price, store id, aisle, and whether they're in stock. These values can
+        also be changed by the user without direct access to the object's variables but rather through 
+        methods that also validate them.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create individual objects, hard-coded
+            Product product1 = new Product("PepsiCo", "Gatorade", 675134, "Beverage", 4.50, 5);
+            Product product2 = new Product("KeurigDrPepper", "A&W Root Beer", 622859, "Beverage", 3.50, 4);
+            Product product3 = new Product("CocaCola", "Fanta", 545877, "Beverage", 3.50, 4);
+            Product product4 = new Product("Kellogg", "Cheez-It", 113086, "Snack", 5.99, 8);
+            Product product5 = new Product("MarsInc", "Combos", 343067, "Snack", 4.99, 8);
+
+            //Create array of the above objects
+            Product[] productArray = new Product[] {product1, product2, product3, product4, product5};
+
+            //Display each of the objects within array
+            Console.WriteLine("Products We Sell \n---------------------");
+            printObjects(productArray); //Print array of objects
+
+            //Edit objects' values, hard-coded; test validations for aisle/id/price
+            productArray[0].ProductPrice = -9; //Outside valid range, should be set to 0 (default)
+            productArray[1].ProductID = 150; //Outside valid range, should be set to 0 (default)
+            productArray[2].SupplierName = "Meijer"; //Should be set properly to name of store
+            productArray[2].ProductName = "Club Soda"; //Should be set properly to store-brand soda
+            productArray[2].ProductPrice = 2.99; //Should be set properly to a lesser value than before
+            productArray[3].InStock(); //In-Stock should be equal to "True"
+            productArray[4].InStock(); //In-Stock should be equal to "True"
+            productArray[4].Aisle = 0; //Outside valid range, but should be set to 0 (default) anyway
+
+            //Display each object in array with changed values
+            Console.WriteLine("\nProducts After Changes \n---------------------");
+            printObjects(productArray); //Print array of objects
+
+            //Display objects in array that are in-stock only
+            Console.WriteLine("\nProducts In Stock \n---------------------");
+            foreach (Product product in productArray)
+            {
+                if(product.IsInStock() == true) //If product's in stock...
+                {
+                    printObjects(product); //Display product's info
+                }
+            }
+
+            //Display objects in array one last time.
+            Console.WriteLine("\nProducts Once More \n---------------------");
+            printObjects(productArray); //Print array of objects
+        }
+
+        public static void printObjects (Array objArray) //For array of objects
+        {
+            foreach (var product in objArray)
+            {
+                Console.WriteLine(product.ToString()); //Display object info
+            }
+        }
+        public static void printObjects(Product productListing) //For single objects
+        {
+            Console.WriteLine(productListing.ToString()); //Display object info
+        }
+    }
+}
