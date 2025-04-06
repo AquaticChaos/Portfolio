48577795a08eddbf32b6440085ec3d06851fc650
Jacob Palmer <63919544+AquaticChaos@users.noreply.github.com>
2025-04-06 19:12:04 -0400

    Add files via upload

---
+++ CSharp_Language/Fall 2022/Lab5/Lab5/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+    CIS-199-50-4228 Lab #5
+    Due: 10/16/2022
+    ID: F2233
+    Purpose: To gather multiple given temperatures within a range (-20 to 130 here) by the user, 
+        then find the average/mean after the user inputs the exit value (999 in this case)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperVal = 0; //The variable input by the user
+            int inputTally = 0; //How many temperatures were inputted (to find mean)
+            int totalVal = 0; //The total value of every temperature value inputted (to find mean)
+
+            const int EXITVAL = 999; //If this input, exit the loop & calculate
+            const int MINVAL = -20, MAXVAL = 130; //Minimum & maximum temperature value (inclusive)
+
+            Console.WriteLine("Enter temperatures from {0} to {1} ({2} to stop)", 
+                    MINVAL, MAXVAL, EXITVAL);
+
+
+            while(temperVal != EXITVAL) //Start loop, continue while value not exit value
+            {
+                Console.Write("Enter temperature: ");
+
+                //Check if input value is valid, set to a boolean
+                Boolean isValid = int.TryParse(Console.ReadLine(), out temperVal);
+
+                if (isValid && temperVal >= MINVAL && temperVal <= MAXVAL)
+                {
+                    //If valid number & within range, add to total & increment tally
+                    totalVal += temperVal;
+                    inputTally++;
+                }
+                else if (isValid && temperVal == EXITVAL)
+                {/*If valid number & equal to exit value, exit while loop*/}
+                else { //If not numerical, or outside of valid range
+                    Console.WriteLine("Valid temperatures range from {0} to {1}." + 
+                        " Please reenter temperature", MINVAL, MAXVAL);
+                }
+            }
+
+            //Display tally & average after exiting loop
+            Console.WriteLine("You entered {0} valid temperatures.", inputTally);
+            double meanVal = Math.Round((double)totalVal / inputTally, 1);
+            Console.WriteLine("The mean temperature is {0} degrees", meanVal);
+        }
+    }
+}
---
+++ CSharp_Language/Fall 2022/Lab6/Lab6/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+    CIS-199-50-4228 Lab/Program/Exam #
+    Due: 10/30/2022
+    ID: F2233
+    Purpose: The purpose of this lab is to use nested for loops to write astericks (stars)
+        to the console based on a labelled pattern A-D (align left increment, left decrement, 
+        right decrement, then right increment)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_STAR = 10; //Maximum amount of stars on a line per pattern
+            int space = 0; //How many spaces to write for patterns C and D
+
+            //PATTERN A, align left increment
+            Console.WriteLine("Pattern A\n");
+            for(int i = 0; i < MAX_STAR; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*"); //Write j amount of stars based on i
+                }
+                Console.WriteLine(); //Write to next line, repeat above
+            }
+
+            //PATTERN B, align left decrement
+            Console.WriteLine("\nPattern B\n");
+            for (int i = MAX_STAR; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*"); //Write j amount of stars based on i
+                }
+                Console.WriteLine(); //Write to next line, repeat above
+            }
+
+            //PATTERN C, align right decrement
+            Console.WriteLine("\nPattern D\n");
+            for (int i = MAX_STAR; i > 0; i--)
+            {
+                space = MAX_STAR - i; //Calculate how many spaces to write per line
+                for(int k = 0; k < space; k++) //Write k amount of spaces based on above variable
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++) //Write j amount of stars based on i
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); //Write to next line, repeat above
+            }
+
+            //PATTERN D, align right increment
+            Console.WriteLine("\nPattern C\n");
+            for (int i = 0; i < MAX_STAR; i++)
+            {
+                space = MAX_STAR - i; //Calculate how many spaces to write per line
+                for (int k = 0; k < space - 1; k++) //Write k amount of spaces based on above variable
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++) //Write j amount of stars based on i
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); //Write to next line, repeat above
+            }
+        }
+    }
+}
